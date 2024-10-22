7cbff109887c3fc1e3b7bed5ac92b49a60f20321
Ondastsaft <115697472+Ondastsaft@users.noreply.github.com>
2024-10-22 23:17:12 +0200

    First version of VerifyAndAttachFile component with model
    First version of VerifyAndAttachFile component with model

---
+++ FormPoC/FormPoC/Components/Pages/Partial/VerifyAndAttachFile/VerifyAndAttachFileController.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace FormPoC.Components.Pages.Partial.VerifyAndAttachFile
+{
+    public class VerifyAndAttachFileController(string verificationCheckBoxTitle, string submitButtonTitle, HttpClient _client, string _uploadUri, string _fileName)
+    {
+        public int Id { get; set; }
+
+        //States
+        public bool IsDisabled = true;
+        private bool hasVerified = false;
+        public bool HasVerified
+        {
+            get => hasVerified;
+            set => hasVerified = value;
+        }
+        public bool HasSubmittedFile { get; private set; }
+
+        //UI Properties
+        public string VerificationCheckBoxTitle { get; private set; } = verificationCheckBoxTitle;
+        public string SubmitButtonTitle { get; private set; } = submitButtonTitle;
+
+        //File upload properties
+        private readonly string uploadUri = _uploadUri;
+        private readonly string fileName = _fileName;
+        HttpClient httpClient = _client;
+        public IBrowserFile SelectedFile { get; private set; }
+
+        // Events
+        public EventCallback<bool> FileSubmitted;
+        public Func<Exception, Task> OnError { get; set; } = async (ex) => await Task.CompletedTask;
+        //TODo AddValidation for model, parentmodel and file
+
+        //Methods
+        public async Task SaveFileToDestinationAsync()
+        {
+            try
+            {
+                var content = new MultipartFormDataContent();
+                var fileContent = new StreamContent(SelectedFile.OpenReadStream(104857600)); 
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue(SelectedFile.ContentType);
+                content.Add(fileContent, fileName, SelectedFile.Name);
+
+                var response = await httpClient.PostAsync(uploadUri, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    HasSubmittedFile = true;
+                    await FileSubmitted.InvokeAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                await OnError.Invoke(ex);
+            }
+        }
+       public async Task HandleFileSelected(InputFileChangeEventArgs e)
+        {
+            SelectedFile = e.File;
+        }
+
+    }
+}
