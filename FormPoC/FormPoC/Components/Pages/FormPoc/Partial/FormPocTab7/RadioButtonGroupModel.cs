5f626d6ef2fca336adf711cbe1cf51c73ddd75d0
Ondastsaft <115697472+Ondastsaft@users.noreply.github.com>
2024-10-22 10:50:08 +0200

    Mid work of implementing Enum

---
+++ FormPoC/FormPoC/Components/Pages/FormPoc/Partial/FormPocTab7/RadioButtonGroupModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormPoC.Components.Pages.FormPoc.Partial.FormPocTab7
+{
+    public class RadioButtonGroupModel<TEnum> where TEnum : Enum
+    {
+        private TEnum _selectedValue;
+        public TEnum SelectedValue
+        {
+            get => _selectedValue;
+            set
+            {
+                if (!_selectedValue.Equals(value))
+                {
+                    _selectedValue = value;
+                    ValueHasChanged?.Invoke();
+                }
+            }
+        }
+        public List<TEnum> radioOptions { get; set; } = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
+        public event Action ValueHasChanged;
+    }
+}
---
+++ FormPoC/FormPoC/Components/Pages/FormPoc/Partial/FormPocTab7/Tab7Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormPoC.Components.Pages.FormPoc.Partial.FormPocTab7
+{
+    public class Tab7Model
+    {
+        public enum Tab7DistanceRadioOptions
+        {
+            short_distance,
+            medium_distance,
+            long_distance
+        }
+        public RadioButtonGroupModel<Tab7DistanceRadioOptions> Tab7DistanceRadioButtonGroupModel { get; set; } = new RadioButtonGroupModel<Tab7DistanceRadioOptions>
+        {
+            SelectedValue = Tab7DistanceRadioOptions.short_distance
+        };
+    }
+}
