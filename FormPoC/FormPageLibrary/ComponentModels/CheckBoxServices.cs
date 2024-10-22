8493a1f2b5399b27750a979aad4109d5612f3f7a
Abdimajid <Abdimajid.mahamed1@gmail.com>
2024-10-22 09:25:06 +0200

    some changes

--- FormPoC/FormPageLibrary/ComponentModels/CheckBoxServices.cs
+++ FormPoC/FormPageLibrary/ComponentModels/CheckBoxServices.cs
@@ -8,6 +8,23 @@ namespace FormPageLibrary.ComponentModels
 {
     public class CheckBoxServices
     {
+        private readonly Dictionary<string, bool> _checkboxVisibilityMap = new();
+        public void UpdateVisibility(string checkboxLabel, bool isChecked)
+        {
+            if (_checkboxVisibilityMap.ContainsKey(checkboxLabel))
+            {
+                _checkboxVisibilityMap[checkboxLabel] = isChecked;
+            }
+            else
+            {
+                _checkboxVisibilityMap.Add(checkboxLabel, isChecked);
+            }
+        }
+
+        public bool IsInputVisible(string checkboxLabel)
+        {
+            return _checkboxVisibilityMap.TryGetValue(checkboxLabel, out var isVisible) && isVisible;
+        }
         public List<CheckBoxModel> CheckBoxes()
         {
             return new List<CheckBoxModel>
